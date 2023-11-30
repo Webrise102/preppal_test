bdb55d6806a3c22edd5a86f48d4adfef4f211b5d
Webrise102 <121498311+Webrise102@users.noreply.github.com>
2023-11-30 19:15:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ const db = mysql.createPool({
 });
 const cron = require("node-cron");
 const fs = require("fs");
-const environment = process.env.ENVIRONMENT || "live";
+const environment = process.env.ENVIRONMENT || "sandbox";
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const endpoint_url =
-  environment === "live"
+  environment === "sandbox"
     ? "https://api-m.sandbox.paypal.com"
     : "https://api-m.paypal.com";
 
